211f53d0e6121513487058dc44eaacf466fedc1c
Sighery <sighery7@gmail.com>
2017-01-25 23:11:29 +0100

    Endless scrolling support
    Upload the testing version with endless scrolling support for people to test it before merging back in master and releasing it.

--- SGImageLoader.user.js
+++ SGImageLoader.user.js
@@ -2,7 +2,7 @@
 // @name         SG OnDemand Image Loader
 // @author       Sighery
 // @description  Stops images from loading all at the same time, and only loads them when you click the View attached image link
-// @version      0.1
+// @version      1.0.0
 // @icon         https://raw.githubusercontent.com/Sighery/Scripts/master/favicon.ico
 // @downloadURL  https://www.github.com/Sighery/Scripts/raw/master/SGImageLoader.user.js
 // @updateURL    https://www.github.com/Sighery/Scripts/raw/master/SGImageLoader.meta.js
@@ -10,24 +10,43 @@
 // @namespace    Sighery
 // @match        https://www.steamgifts.com/giveaway/*
 // @match        https://www.steamgifts.com/discussion/*
-// @match        https://www.steamgifts.com/trade/*
 // @match        https://www.steamgifts.com/messages
-// @run-at       document-end
 // ==/UserScript==
 
-stop_loading();
+// ==================== SETTING UP DATA ====================
+// ========== USER EDITABLE ==========
+// 0 Extended SteamGifts, 1 SG++; 2 Revilheart's Script; 3 SGT Frog
+var endless_type = null;
 
-function stop_loading() {
-	// Find all divs with the following class, and stop the img they contain from loading by removing the src attribute if it hasn't been loaded yet
-	var elements = document.getElementsByClassName("comment__toggle-attached");
-	for (var i = 0; i < elements.length; i++) {
-		var img = elements[i].parentElement.getElementsByTagName("IMG")[0];
-		if (img.complete === false) {
-			img.src = "";
-			add_trigger(elements[i]);
-		}
+
+
+// ==================== MAIN ====================
+if (endless_type !== null) {
+	if (window.location.href === "https://www.steamgifts.com/messages") {
+		detect_mutations_inbox_messages(endless_type);
+	} else if (window.location.href.indexOf("https://www.steamgifts.com/discussion/") !== -1 || window.location.href.indexOf("https://www.steamgifts.com/giveaway/") !== -1) {
+		detect_mutations_threads_giveaways(endless_type);
 	}
 }
+
+var elements = document.getElementsByClassName("comment__toggle-attached");
+for (var i = 0; i < elements.length; i++) {
+	stop_loading();
+}
+
+
+
+// ==================== FUNCTIONALITY ====================
+// ========== MODIFYING FUNCTIONS ==========
+function stop_loading(element) {
+	// Stop the img from loading by removing the src attribute if it hasn't been loaded yet
+	var img = element.parentElement.getElementsByTagName("IMG")[0];
+	if (img.complete === false) {
+		img.src = "";
+		add_trigger(element);
+	}
+}
+
 
 function add_trigger(element) {
 	// This should receive the element with class "comment__toggle-attached"
@@ -38,4 +57,103 @@ function add_trigger(element) {
 			img.src = link.href;
 		}
 	});
+}
+
+
+
+// ========== MUTATION FUNCTIONS ==========
+function detect_mutations_threads_giveaways(endless_type) {
+	var target = document.getElementsByClassName("sidebar")[0].nextElementSibling;
+	var config = {
+		"subtree": true,
+		"childList": true,
+	};
+
+	var observer = new MutationObserver(function(mutations) {
+		mutations.forEach(function(mutation) {
+			var elements;
+			var i;
+			var j;
+
+			if (endless_type === 0) {
+				// Extended SteamGifts
+				if (mutation.addedNodes.length === 2 && mutation.addedNodes[0].className === "page__heading" && mutation.addedNodes[1].className === "comments") {
+					elements = mutation.addedNodes[1].getElementsByClassName("comment__toggle-attached");
+
+					for(i = 0; i < elements.length; i++) {
+						stop_loading(elements[i]);
+					}
+				}
+			} else if (endless_type === 1) {
+				// SteamGifts++
+				if (mutation.addedNodes.length === 1 && mutation.addedNodes[0].className === "comment") {
+					elements = mutation.addedNodes[0].getElementsByClassName("comment__toggle-attached");
+
+					for(i = 0; i < elements.length; i++) {
+						stop_loading(elements[i]);
+					}
+				}
+			} else if (endless_type === 2 || endless_type === 3) {
+				// Revilheart's script (2) or SGT Frog (3)
+				if (mutation.addedNodes.length > 0) {
+					for(j = 0; j < mutation.addedNodes.length; j++) {
+						if (mutation.addedNodes[i].className === "comment") {
+							elements = mutation.addedNodes[i].getElementsByClassName("comment__toggle-attached");
+
+							for(i = 0; i < elements.length; i++) {
+								stop_loading(elements[i]);
+							}
+						}
+					}
+				}
+			}
+		});
+	});
+
+	observer.observe(target, config);
+}
+
+
+function detect_mutations_inbox_messages(endless_type) {
+	// SG++ has no endless scrolling feature here
+	// SGT Frog has no endless scrolling feature here
+	var target = document.getElementsByClassName("sidebar")[0].nextElementSibling;
+	var config = {
+		"subtree": true,
+		"childList": true,
+	};
+
+	var observer = new MutationObserver(function(mutations) {
+		mutations.forEach(function(mutation) {
+			var elements;
+			var i;
+			var j;
+
+			if (endless_type === 0) {
+				// Extended SteamGifts
+				if (mutation.addedNodes.length === 2 && mutation.addedNodes[0].className === "page__heading" && mutation.addedNodes[1].tagName === "DIV") {
+					elements = mutation.addedNodes[1].getElementsByClassName("comment__toggle-attached");
+
+					for(i = 0; i < elements.length; i++) {
+						stop_loading(elements[i]);
+					}
+				}
+			} else if (endless_type === 2) {
+				// Revilheart's script
+				if (mutation.addedNodes.length > 0) {
+					for(j = 0; j < mutation.addedNodes.length; j++) {
+						if (mutation.addedNodes[i].className === "comments__entity" || mutation.addedNodes[i].className === "comments") {
+							elements = mutation.addedNodes[i].getElementsByClassName("comment__toggle-attached");
+
+							for(i = 0; i < elements.length; i++) {
+								stop_loading(elements[i]);
+							}
+						}
+					}
+				}
+			}
+		});
+	});
+
+	observer.observe(target, config);
 }
